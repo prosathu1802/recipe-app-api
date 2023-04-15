ed5c201270052f5719cede051027587e97b9cb75
Hin Pham <hinphamd0218@gmail.com>
2023-04-15 17:15:23 -0500

    End of section 8

---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+"""Sample Test  """
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc modules"""
+
+    def test_add_numbers(self):
+        result = calc.add(5, 6)
+        self.assertEqual(result, 11)
+
+    def test_subtract_numbers(self):
+        result = calc.subtract(10, 15)
+
+        self.assertEqual(result, 5)
