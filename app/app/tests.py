30507a9a9cec9104ea4fffb122dd363853e95cee
Hin Pham <hinphamd0218@gmail.com>
2023-04-15 21:43:30 -0500

    End of section 9

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,12 +4,14 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc modules"""
 
     def test_add_numbers(self):
         result = calc.add(5, 6)
         self.assertEqual(result, 11)
+
 
     def test_subtract_numbers(self):
         result = calc.subtract(10, 15)
